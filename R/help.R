057ab8f1126490eb11f3a5f107272890de046f1c
Daniel Schulman <danieljschulman@gmail.com>
2012-02-09 12:19:50 -0500

    basic packaging and documentation

---
+++ R/help.R
@@ -0,0 +1,17 @@
+#' Bootstrapped confidence intervals for linear mixed-effect regression
+#'
+#' lmerbootci provides easy methods of performing bootstrap
+#' resampling on linear mixed-effect regression models fit with the lme4
+#' package.  It implements two types of resampling: a fully parametric
+#' bootstrap, and a semiparametric bootstrap which resamples residuals.
+#'
+#' The core method is \code{\link{lmer.boot}}: given a model object
+#' returned by \code{\link[lme4]{lmer}} or related methods, it
+#' produces a bootstrapped sample suitable for use with 
+#' \code{\link[boot]{boot.ci}} and other methods of the boot package.
+#'
+#' @docType package
+#' @name lmerbootci
+#' @aliases lmerbootci lmerbootci-package
+#' @importMethodsFrom Matrix t
+NULL
--- R/lmerbootci.R
+++ R/lmerbootci.R
@@ -1,3 +1,25 @@
+#' Shrinkage-corrected estimates of random effects
+#'
+#' Give estimates of the random effects from a fitted
+#' model produced by \code{\link[lme4]{lmer}} or related 
+#' functions.
+#'
+#' Relative to the shrinkage estimates given by 
+#' \code{\link[lme4]{ranef}}, these have been transformed 
+#' so that the (co)variance of the estimates matches 
+#' the estimated (co)variance of the random-effect terms.
+#' They should be more suitable for use in bootstrapping or
+#' other resampling.
+#' 
+#' @param m A fitted model of type \code{\link[lme4]{mer-class}}
+#' @return A list of data frames, one for each grouping factor
+#'   for the random effects.
+#' @export
+#' @references Carpenter, J. R., Goldstein, H., and Rasbash, J. 
+#'   (2003). A novel bootstrap procedure for assessing the 
+#'   relationship between class size and achievement. Journal of the
+#'   Royal Statistical Society: Series C (Applied Statistics), 
+#'   52(4):431-443.
 ranef.reflate <- function(m) {
   mapply(function(vc.est, resid) {
     u <- as.matrix(resid)
@@ -11,6 +33,17 @@ sample.rows <- function(df, size=nrow(df), replace=F, prob=NULL) {
   df[sample.int(nrow(df), size, replace, prob),]
 }
 
+#' Resample residuals of a mixed-effect model
+#'
+#' Generates a new response vector for a fitted mixed-effect model
+#' by resampling residuals at all levels (including all random effects).
+#' All residuals are adjusted to match their (co)variance to the
+#' variance and covariance estimates of the model.
+#'
+#' @param m A fitted model of type \code{\link[lme4]{mer-class}}
+#' @return A numeric vector of the same length as the original response
+#' @seealso \code{\link{ranef.reflate}}
+#' @export
 resamp.resid <- function(m) {
   resid.scaled <- attr(VarCorr(m), 'sc') * resid(m) / sd(resid(m))
   resid.boot <- sample(resid.scaled, replace=T)
@@ -21,6 +54,24 @@ resamp.resid <- function(m) {
   as.vector(eta.fix + eta.ranef + resid.boot)
 }
 
+#' Bootstrap resampling of a mixed-effect model
+#'
+#' Generates \code{R} bootstrap replicates of the parameter estimates
+#' of a mixed-effect regression model.  Two types of resampling are
+#' supported: a fully parametric bootstrap, and a semiparametric
+#' bootstrap that resamples residuals and random effects.
+#'
+#' @param m A fitted model of type \code{\link[lme4]{mer-class}}
+#' @param R The number of bootstrap replicates
+#' @param type The resampling scheme to use.  Defaults to parametric. 
+#' @param ... Additional arguments, passed to \code{\link[boot]{boot}}.
+#' @return An object of class \code{\link[boot]{boot}}
+#' @references Carpenter, J. R., Goldstein, H., and Rasbash, J. 
+#'   (2003). A novel bootstrap procedure for assessing the 
+#'   relationship between class size and achievement. Journal of the
+#'   Royal Statistical Society: Series C (Applied Statistics), 
+#'   52(4):431-443.
+#' @export
 lmer.boot <- function(m, R, type=c('parametric','residuals'), ...) {
   type <- match.arg(type)
   boot(
